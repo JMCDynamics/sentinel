b85b7da7e517d69ef6d45dbb977c4d1f1faf6389
João Mateus <mateusgamescoelho547@gmail.com>
2025-12-14 16:41:15 -0300

    fix: autocapture posthog

--- landing-page/app/provider.tsx
+++ landing-page/app/provider.tsx
@@ -8,6 +8,7 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST!,
       defaults: "2025-11-30",
+      autocapture: true,
     });
   }, []);
 
